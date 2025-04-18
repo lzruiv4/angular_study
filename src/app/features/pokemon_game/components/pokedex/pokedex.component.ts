779db0673208525c9d420fb6cc7846b37bd3e677
lzruiv4 <lzruiv4@gmail.com>
2025-04-19 00:31:41 +0200

    20250419 :: pokedex work

--- src/app/features/pokemon_game/components/pokedex/pokedex.component.ts
+++ src/app/features/pokemon_game/components/pokedex/pokedex.component.ts
@@ -2,20 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { IPokemon } from '../../../../shared/models/IPokemen.model';
 import { CommonModule } from '@angular/common';
 import { PokemonService } from '../../services/pokemon.service';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   standalone: true, // After angular 17 can one use this. But module is not used.
   selector: 'app-pokedex',
-  imports: [CommonModule],
+  imports: [CommonModule, NzTableModule],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.css',
 })
 export class PokedexComponent implements OnInit {
   pokemons: IPokemon[] = [];
+
+  pokemonsInList: IPokemon[] = [];
+  // loading = true;
+  // total = 1;
+  // pageSize = 10;
+  // pageIndex = 1;
+
   constructor(private pokemonService: PokemonService) {}
   ngOnInit(): void {
     this.pokemonService.getPokemonDTOs().subscribe((data) => {
       this.pokemons = data;
     });
+  }
+  onCurrentPageDataChange(data: readonly IPokemon[]) {
+    this.pokemonsInList = [...data];
   }
 }
