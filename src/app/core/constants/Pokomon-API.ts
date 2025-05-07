2c91d32c9d8ce424a716f309c0d957a14a3e1aca
lzruiv4 <lzruiv4@gmail.com>
2025-05-07 14:15:48 +0200

    20250507 :: update css

--- src/app/core/constants/Pokomon-API.ts
+++ src/app/core/constants/Pokomon-API.ts
@@ -5,7 +5,7 @@ export const POKEMON_API =
   'https://pokeapi.co/api/v2/pokemon?limit=' + POKEMON_AMOUNT;
 
 // json backend
-// export const POKEMON_RECORDS_API = 'http://localhost:9009/pokemonRecords';
+export const POKEMON_RECORDS_API = 'http://localhost:9009/pokemonRecords';
 
 // SPRINGBOOT BACKEND
-export const POKEMON_RECORDS_API = 'http://localhost:9090/api/pokemonRecords';
+// export const POKEMON_RECORDS_API = 'http://localhost:9090/api/pokemonRecords';
--- src/app/core/constants/User-API.ts
+++ src/app/core/constants/User-API.ts
@@ -1,10 +1,10 @@
 // Json server as backend
-// export const USER_API = 'http://localhost:9009/users';
-// export const TEST_USER_ID = 'user2';
+export const USER_API = 'http://localhost:9009/users';
+export const TEST_USER_ID = 'user2';
 // export const TEST_USER_ID = 'ad1';
 
 // Springboot as backen
-export const USER_API = 'http://localhost:9090/api/users';
-export const TEST_USER_ID = '796493a8-3967-4f6a-abd4-f8a9a9509062';
+// export const USER_API = 'http://localhost:9090/api/users';
+// export const TEST_USER_ID = '796493a8-3967-4f6a-abd4-f8a9a9509062';
 
 export const CURRENT_USER_ID = TEST_USER_ID;
