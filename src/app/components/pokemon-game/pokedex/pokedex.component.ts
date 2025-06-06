00d3598657a3b4170d3c2731b803bfee4c5a5950
lzruiv4 <lzruiv4@gmail.com>
2025-06-07 01:46:18 +0200

    20250607 :: refactoring pokedex component with destory #15

--- src/app/components/pokemon-game/pokedex/pokedex.component.ts
+++ src/app/components/pokemon-game/pokedex/pokedex.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IPokemonWithNameAndFotos } from '../../../models/IPokemen.model';
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { ImageComponent } from '@/shared/base-components/image/image.component';
 import { PokemonService } from '@/services/pokemon.service';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 @Component({
   standalone: true, // After angular 17 can one use this. And module is not used.
@@ -12,8 +13,12 @@ import { PokemonService } from '@/services/pokemon.service';
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.less',
 })
-export class PokedexComponent implements OnInit {
-  pokemons: IPokemonWithNameAndFotos[] = [];
+export class PokedexComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject<void>();
+
+  get pokemons$(): Observable<IPokemonWithNameAndFotos[]> {
+    return this.pokemonService.pokemons$;
+  }
 
   selectedPokemon: IPokemonWithNameAndFotos = {
     id: '',
@@ -29,7 +34,14 @@ export class PokedexComponent implements OnInit {
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    this.pokemonService.getPokemonDTOs().subscribe();
-    this.pokemonService.pokemons$.subscribe((data) => (this.pokemons = data));
+    this.pokemonService
+      .getPokemonDTOs()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
